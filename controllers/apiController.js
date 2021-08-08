bfff5d80753186af351c0d7e9d4486ccea998021
azzzulya <azisulyagifari27@gmail.com>
2021-08-08 13:45:28 +0700

    detail page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,7 @@ const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Traveler = require('../models/Booking')
 const Category = require('../models/Category')
+const Bank = require('../models/Bank')
 
 module.exports = {
   landingPage : async  (req, res ) => {
@@ -67,6 +68,37 @@ module.exports = {
     catch(error){
       console.log(error)
       res.status(500).json({ message: "Internal server error"})
+    }
+  },
+
+  detailPage: async (req, res) => {
+    try{
+      const { id } = req.params;
+      const item = await Item.findOne({_id: id})
+        .populate({path:'imageId', select:'_id name qty imageUrl'})
+        .populate({path:'activityId', select:'_id name type imageUrl'})
+        .populate({path:'imageId', select:'_id imageUrl'});
+
+      const bank = await Bank.find();
+
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content: "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer"
+      }
+
+      res.status(200).json({ 
+        ...item._doc,
+        testimonial,
+        bank
+      })
+    }
+    catch(error){
+
     }
   }
 }
